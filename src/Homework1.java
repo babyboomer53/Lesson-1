585a994bc25de4a835614ac161e005058710deba
Edgar Cole <edgarcolejr@gmail.com>
2021-06-29 16:18:11 -0700

    I think it's time to take a checkpoint.

--- src/Homework1.java
+++ src/Homework1.java
@@ -14,11 +14,25 @@ public class Homework1 {
         return temporaryArray;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] arguments) {
         SecureRandom generateRandomNumber = new SecureRandom();
-        final int NUM_READINGS = 60;
+        final int NUM_READINGS = 6;
         final int INSERTS_PER_READING = 1000;
         int[] array = {0};
+        int index;
+        int value;
+        for (int readings = 1; readings <= NUM_READINGS; readings++) {
+            long startTime = System.nanoTime();
+            for (int inserts = 1; inserts <= INSERTS_PER_READING; inserts++) {
+                index = generateRandomNumber.nextInt(array.length);
+                value = generateRandomNumber.nextInt();
+                array = Homework1.insert(array, index, value);
+            }
+            long stopTime = System.nanoTime();
+            double timePerReading = (double) (stopTime - startTime) / INSERTS_PER_READING;
+            System.out.printf("%10d", array.length);
+            System.out.println();
+        }
     }
 
 }
