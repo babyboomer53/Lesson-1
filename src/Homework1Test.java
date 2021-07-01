f7ccf1b6cdf66e667de02b59489a925d447bb0d3
Edgar Cole <edgarcolejr@gmail.com>
2021-06-30 19:27:26 -0700

    Added another test method.

--- src/Homework1Test.java
+++ src/Homework1Test.java
@@ -9,7 +9,13 @@ public class Homework1Test {
     public void insertValueIntoArray() {
         int[] firstArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int[] secondArray = {1, 2, 3, 4, 6, 7, 8, 9};
-        int[]result = Homework1.insert(secondArray,4,5);
-        assertTrue(Arrays.equals(firstArray,result));
+        int[] result = Homework1.insert(secondArray, 4, 5);
+        assertTrue(Arrays.equals(firstArray, result));
+    }
+
+    @Test(expectedExceptions = IndexOutOfBoundsException.class)
+    public void invalidIndex() {
+        int[] secondArray = {1, 2, 3, 4, 6, 7, 8, 9};
+        int[] result = Homework1.insert(secondArray, 9, 5);
     }
 }
